8eb0c298b60131cfb2b945bdcd966ca3f9f81ff1
Sid Sancheti <sidsancheti94@gmail.com>
2024-10-25 18:40:05 -0400

    Added name

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
   const profileData = [
     { name: "Ryan(Demo)", imageSrc: "/ryanpic.jpg" },  // Feel free to remove demo page whenever you want
     { name: "Ethna Wang", imageSrc: "/defaultpfp.jpg" },
-    { name: "Person2", imageSrc: "/defaultpfp.jpg" },
+    { name: "Sid Sancheti", imageSrc: "/defaultpfp.jpg" },
     { name: "Nathan Wan", imageSrc: "/defaultpfp.jpg" },
     { name: "Person4", imageSrc: "/defaultpfp.jpg" },
     { name: "Michael Li", imageSrc: "/defaultpfp.jpg" },
