c824649e6f3708b48479629247bd99080241ef8c
michaelli-radianhub <michael.li@radianhub.com>
2024-10-25 18:45:08 -0400

    nevermind

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,12 +9,16 @@ export default function Home() {
   // Names and images for each profile 
   const profileData = [
     { name: "Ryan(Demo)", imageSrc: "/ryanpic.jpg" },  // Feel free to remove demo page whenever you want
-    { name: "Ethna Wang", imageSrc: "/EthanWangProfile.jpg" },
+    { name: "Ethna Wang", imageSrc: "/defaultpfp.jpg" },
     { name: "Sid Sancheti", imageSrc: "/defaultpfp.jpg" },
     { name: "Nathan Wan", imageSrc: "/defaultpfp.jpg" },
     { name: "Person4", imageSrc: "/defaultpfp.jpg" },
     { name: "Michael Li", imageSrc: "/defaultpfp.jpg" },
     { name: "Person6", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person7", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person8", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person9", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person10", imageSrc: "/defaultpfp.jpg" }
   ];
 
   const toggleOutline = () => {
