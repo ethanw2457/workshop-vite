6a926ed6fef15f56ba725a1c4d01074315efaddd
michaelli-radianhub <michael.li@radianhub.com>
2024-10-25 18:40:18 -0400

    f

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,6 +15,10 @@ export default function Home() {
     { name: "Person4", imageSrc: "/defaultpfp.jpg" },
     { name: "Michael Li", imageSrc: "/defaultpfp.jpg" },
     { name: "Person6", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person7", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person8", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person9", imageSrc: "/defaultpfp.jpg" },
+    { name: "Person10", imageSrc: "/defaultpfp.jpg" }
   ];
 
   const toggleOutline = () => {
